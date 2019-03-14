3ab65742f0a82c4eb8406d894d720095647651a8
bbchip0103 <bbchip13@gmail.com>
2019-03-14 13:19:06 +0900

    Update range source

--- range_update_server_sheet.py
+++ range_update_server_sheet.py
@@ -37,66 +37,67 @@ def get_auth():
             pickle.dump(creds, token)
     return creds
 
-def get_value_from_sheet(sheet, sheet_id, range_):
+def get_values_from_sheet(sheet, sheet_id, range_):
     # Call the Sheets API
     result = sheet.values().get(spreadsheetId=sheet_id, range=range_).execute()
     value = result.get('values', [])
-    return value[0][0] if value else None
+    return value if value else None
 
 def ping_server(target_ip):
-    if not target_ip or not re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', target_ip):
+    if not target_ip or not re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', target_ip[0]):
         return
-    ping_status = sp.call(['ping', target_ip], stdout=sp.DEVNULL)
-    # ping_status = sp.call(['ping', '-c', '5', '-q', target_ip], stdout=sp.DEVNULL)
+    ping_status = sp.call(['ping', target_ip[0]], stdout=sp.DEVNULL)
+    # ping_status = sp.call(['ping', '-c', '5', '-q', target_ip[0]], stdout=sp.DEVNULL)
 
     return ping_status
 
-def update_sheet_status(sheet, sheet_id, range_, text):
-    values = [ [ text ] ]
+def update_sheet_status(sheet, sheet_id, range_, status, modified_time):
+    values = [ [status, modified_time] ]
     body = { 'values': values }
     result = sheet.values().update(
                 spreadsheetId=sheet_id, range=range_,
                 valueInputOption='USER_ENTERED', body=body).execute()
     return result
 
-def update_server_sheet(range_numb, sheet, sheet_id, sheet_name):
-    target_ip_range = sheet_name+'!'+'F'+str(range_numb)
-    target_ip = get_value_from_sheet(sheet, sheet_id, target_ip_range)
+def update_server_sheet(target_info, sheet, sheet_id, sheet_name):
+    target_ip, target_range = target_info
+    target_status_range = '{}!G{}:H{}'.format(sheet_name, target_range, target_range)
     ping_status = ping_server(target_ip)
     if ping_status == None:
         return
-
-    target_status_range = sheet_name+'!'+'G'+str(range_numb)
     if ping_status == 0:
         update_sheet_status(sheet, sheet_id, 
-                            target_status_range, 'ON')
+                            target_status_range, 'ON', str(datetime.now()))
     else:
         update_sheet_status(sheet, sheet_id, 
-                            target_status_range, 'OFF')
-
-    modified_time_range = sheet_name+'!'+'H'+str(range_numb)
-    update_sheet_status(sheet, sheet_id, 
-                        modified_time_range, str(datetime.now()))
+                            target_status_range, 'OFF', str(datetime.now()))
 
 def single_update_server_sheet(sheet_id, sheet_name, start_range, end_range):
     creds = get_auth()
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
 
-    for range_numb in range(start_range, end_range+1):
-        update_server_sheet(range_numb, sheet, sheet_id, sheet_name)
+    target_ip_range = '{}!F{}:F{}'.format(sheet_name, start_range, end_range)
+    target_ip_list = get_values_from_sheet(sheet, sheet_id, target_ip_range)
+
+    for target_info in zip(target_ip_list, range(start_range, end_range+1)):
+        update_server_sheet(target_info, sheet, sheet_id, sheet_name)
 
 def multi_update_server_sheet(sheet_id, sheet_name, start_range, end_range):
     creds = get_auth()
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
 
+    target_ip_range = '{}!F{}:F{}'.format(sheet_name, start_range, end_range)
+    target_ip_list = get_values_from_sheet(sheet, sheet_id, target_ip_range)
+
     with Pool(processes=4) as pool:
         f_update_server_sheet = partial(update_server_sheet, 
                                             sheet=sheet, 
                                             sheet_id=sheet_id, 
                                             sheet_name=sheet_name)
-        pool.map(f_update_server_sheet, list(range(start_range, end_range+1)))
+        pool.map(f_update_server_sheet, 
+                    list(zip(target_ip_list, range(start_range, end_range+1))))
 
 if __name__ == '__main__':
     # The ID and range of a sample spreadsheet.
@@ -110,10 +111,11 @@ if __name__ == '__main__':
         print('Server check start!')
         print()
 
+        # single_update_server_sheet(SPREADSHEET_ID, SHEET_NAME, START_RANGE, END_RANGE)
         multi_update_server_sheet(SPREADSHEET_ID, SHEET_NAME, START_RANGE, END_RANGE)
 
         print(str(datetime.now()))
         print('Server check end.')
         print()
 
-        time.sleep(10)
+        time.sleep(4)
